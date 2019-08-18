b51ed388752640dc5cff0a24680ea57e4710df43
Trinh Huu Duc <yellowthundercat@gmail.com>
2019-08-18 17:07:33 +0700

    scoreboard

---
+++ app/src/main/java/com/example/vocarun/Score.java
@@ -0,0 +1,14 @@
+package com.example.vocarun;
+
+import java.util.List;
+
+class Score {
+    public String name;
+    public int point;
+
+
+    public Score(String name, int point) {
+        this.name = name;
+        this.point = point;
+    }
+}
